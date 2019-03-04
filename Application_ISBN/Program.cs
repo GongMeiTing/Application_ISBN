8ea84b014281dcbc2f2d24a3bb7893f9c97f2885
meitingtonia <meitingtonia@gmail.com>
2019-03-04 10:38:04 +0800

    my code about ISBN

--- Application_ISBN/Program.cs
+++ Application_ISBN/Program.cs
@@ -10,6 +10,50 @@ namespace Application_ISBN
     {
         static void Main(string[] args)
         {
+            string input;
+            int sum = 0;
+            int multiplier = 10;
+            int lastdigit = 0;
+            string ISBN;
+            string preISBN="";
+            Console.Write("\n Please enter the productID:");
+            input = Console.ReadLine().ToString();                   
+            List<int> IDList = new List<int>();
+            List<int> list = new List<int>();
+            for(int i = 3; i < input.Length; i++)
+            {
+                int number = int.Parse(input[i].ToString());
+                int bigNum = number * multiplier;
+                IDList.Add(number);
+                list.Add(bigNum);
+                multiplier--;
+            }
+          
+            for(int i = 0; i < list.Count; i++)
+            {
+                sum = sum + list[i];
+            }
+            while ((sum+lastdigit)%11!=0)
+            {
+                lastdigit++;             
+            }
+            
+            for(int i = 0; i < IDList.Count; i++)
+            {
+              
+                string number = IDList[i].ToString();
+                preISBN = preISBN + number;
+               
+            }
+            if (lastdigit != 10)
+            {
+                ISBN = preISBN + lastdigit.ToString();
+            }
+            else
+            {
+                ISBN = preISBN + "X";
+            }
+            Console.WriteLine("The ISBN number is:"+" "+ISBN);
         }
     }
 }
